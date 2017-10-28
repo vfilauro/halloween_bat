f85d44633a2fba8a1f532b4e34298c81e7cb7419
Valerio Filauro <valerio.filauro@hotmail.com>
2017-10-28 15:28:06 -0700

    fixes

--- halloween_bat.ino
+++ halloween_bat.ino
@@ -7,10 +7,10 @@
 
 
 //#define DEBUG
-#define ServoPin 3
-#define MotionSensorPin 5 
-#define MOTION_COUNT_TRIGGER 5 //how mny times motion is to be detected in order to trigger an action
-#define LedPin 6
+#define ServoPin D3
+#define MotionSensorPin D5 
+#define MOTION_COUNT_TRIGGER 1 //how mny times motion is to be detected in order to trigger an action
+#define LedPin D6
 #define NUMLEDS 0
 #define MAX_STEPS_IN_SEQ 10
 
@@ -25,60 +25,20 @@ struct sequence {
 };
 
 
-SoftwareSerial mySoftwareSerial(1, 2); // RX, TX
+SoftwareSerial mySoftwareSerial(D2, D1); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 Servo myServo;
 
-#define NO_OF_SEQS 6
-sequence dog[NO_OF_SEQS] =
+#define NO_OF_SEQS 1
+sequence bat[NO_OF_SEQS] =
 {
 	{
-		8,
-		2,	//Devil
-		{ 185, 0, 0 },
-		{ 10, 0, 0 },
-		{ 70, 35, 70, 40, 20, 60, 30, 10 },
-		{ 50, 30, 25, 35, 20, 0, 120, 120 }
-	},
-	{
-		8,
-		4,	//Growl 1
-		{ 1, 0, 0 },
-		{ 1, 0, 0 },
-		{ 5, 10, 4, 11, 5, 2, 15, 3 },
-		{ 40, 40, 40, 30, 30, 15, 85, 5 }
-	},
-	{
-		8,
-		1,	//Growl & Bark 1
-		{ 60, 0, 0 },
-		{ 20, 0, 0 },
-		{ 6,	17,		9,		54,		10,		21,		2,	10 },
-		{ 175,	25,		170,	15,		60,		100,	25,	40 }
-	},
-	{
-		10,
-		5,	//Growl 2
-		{ 10, 0, 0 },
-		{ 1, 0, 0 },
-		{ 5, 10, 4, 11, 4, 9, 2, 8,	4,	10 },
-		{ 35, 35, 35, 35, 35, 35, 35, 35, 30, 35 }
-	},
-	{
-		10,
-		6,	//Growl & Bark 2
-		{ 100, 0, 0 },
-		{ 10, 0, 0 },
-		{ 15,	3,	54,		0,		45,		65,		31,		6,		20,		5 },
-		{ 160,	5,	30,		127,	15,		90,		30,		110,	240,	10 }
-	},
-	{
-		4,
-		3,	//Bark
-		{ 10, 10, 10 },
-		{ 1, 1, 1 },
-		{ 5, 70, 10, 2 },
-		{ 7, 20, 20, 0 }
+		3,
+		2,
+		{ 0, 0, 0 },
+		{ 0, 0, 0 },
+		{ 180,	180, 0 },
+		{ 255,	255, 255 }
 	}
 };
 
@@ -91,7 +51,11 @@ sequence dog[NO_OF_SEQS] =
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMLEDS, LedPin, NEO_GRB + NEO_KHZ800);
+
+
+// Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMLEDS, LedPin, NEO_GRB + NEO_KHZ800);
+
+
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
@@ -104,9 +68,9 @@ void setup()
 #ifdef DEBUG
 	Serial.begin(115200);
 	Serial.println();
-	Serial.println(F("DFRobot DFPlayer Mini Demo"));
 	Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
 #endif
+
 
 	while (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
 #ifdef DEBUG
@@ -117,11 +81,12 @@ void setup()
 		delay(1000);
 	}
 
+
 #ifdef DEBUG
 	Serial.println(F("DFPlayer Mini online."));
 #endif
 
-	myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+	myDFPlayer.volume(20);  //Set volume value. From 0 to 30
 	myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
 
 #ifdef DEBUG
@@ -139,8 +104,12 @@ void setup()
 	Serial.print("File Counts in Folder 1: ");
 	Serial.println(myDFPlayer.readFileCountsInFolder(1)); //read fill counts in folder SD:/03
 
+
+
+
 	Serial.println(F("Initializing Motion Sensor"));
 #endif
+
 
 	pinMode(MotionSensorPin, INPUT);
 
@@ -151,10 +120,11 @@ void setup()
 	myServo.attach(ServoPin);
 	myServo.write(0);              // reset servo position
 	delay(15);                       // waits 15ms for the servo to reach the position  
-	strip.begin();
-	strip.show(); // Initialize all pixels to 'off'
+//	strip.begin();
+//	strip.show(); // Initialize all pixels to 'off'
 
 	randomSeed(analogRead(A0)); //initialize random number generator
+
 
 }
 
@@ -168,39 +138,40 @@ void loop() {
 	byte active_seq;
 	int motion = digitalRead(MotionSensorPin); // Read digital OUT value
 
-	if (motion)
+	if (motion) {
 		motion_count++;
+	}
 	else
 		motion_count = 0;
 
 
 	if (motion_count == MOTION_COUNT_TRIGGER)
 	{
-		active_seq = random(0, NO_OF_SEQS);
+		//active_seq = random(0, NO_OF_SEQS);
 		//active_seq = seq_count++%NO_OF_SEQS;
-		//active_seq = 5;
+		active_seq = 0;
 
-		myDFPlayer.playFolder(1, dog[active_seq].audioclip);  //play specific mp3 in SD:/15/004.mp3; Folder Name(1~99); File Name(1~255)    
+		myDFPlayer.playFolder(1, bat[active_seq].audioclip);  //play specific mp3 in SD:/15/004.mp3; Folder Name(1~99); File Name(1~255)    
 
-		strip.setPixelColor(0, strip.Color(dog[active_seq].col_ON[0], dog[active_seq].col_ON[1], dog[active_seq].col_ON[2]));
-		strip.setPixelColor(1, strip.Color(dog[active_seq].col_ON[0], dog[active_seq].col_ON[1], dog[active_seq].col_ON[2]));
-		strip.show(); // This sends the updated pixel color to the hardware.
-		for (int i = 0; i < dog[active_seq].no_of_steps - 1; i++)
+//		strip.setPixelColor(0, strip.Color(bat[active_seq].col_ON[0], bat[active_seq].col_ON[1], bat[active_seq].col_ON[2]));
+//		strip.setPixelColor(1, strip.Color(bat[active_seq].col_ON[0], bat[active_seq].col_ON[1], bat[active_seq].col_ON[2]));
+//		strip.show(); // This sends the updated pixel color to the hardware.
+		for (int i = 0; i < bat[active_seq].no_of_steps - 1; i++)
 		{
-			myServo.write(dog[active_seq].pos[i]);              // tell servo to go to position in variable 'pos'
-			delay(dog[active_seq].del[i] * 10);
+			myServo.write(bat[active_seq].pos[i]);              // tell servo to go to position in variable 'pos'
+			delay(bat[active_seq].del[i] * 10);
 		}
 
-		strip.setPixelColor(0, strip.Color(dog[active_seq].col_SBY[0], dog[active_seq].col_SBY[1], dog[active_seq].col_SBY[2]));
-		strip.setPixelColor(1, strip.Color(dog[active_seq].col_SBY[0], dog[active_seq].col_SBY[1], dog[active_seq].col_SBY[2]));
-		strip.show(); // This sends the updated pixel color to the hardware.
-		myServo.write(dog[active_seq].pos[dog[active_seq].no_of_steps - 1]);              // tell servo to go to position in variable 'pos'
-		delay(dog[active_seq].del[dog[active_seq].no_of_steps - 1] * 10);
+//		strip.setPixelColor(0, strip.Color(bat[active_seq].col_SBY[0], bat[active_seq].col_SBY[1], bat[active_seq].col_SBY[2]));
+//		strip.setPixelColor(1, strip.Color(bat[active_seq].col_SBY[0], bat[active_seq].col_SBY[1], bat[active_seq].col_SBY[2]));
+//		strip.show(); // This sends the updated pixel color to the hardware.
+		myServo.write(bat[active_seq].pos[bat[active_seq].no_of_steps - 1]);              // tell servo to go to position in variable 'pos'
+		delay(bat[active_seq].del[bat[active_seq].no_of_steps - 1] * 10);
 
 		myServo.write(0);
-		strip.setPixelColor(0, strip.Color(0, 0, 0));
-		strip.setPixelColor(1, strip.Color(0, 0, 0));
-		strip.show(); // This sends the updated pixel color to the hardware.
+//		strip.setPixelColor(0, strip.Color(0, 0, 0));
+//		strip.setPixelColor(1, strip.Color(0, 0, 0));
+//		strip.show(); // This sends the updated pixel color to the hardware.
 		delay(6000);
 
 	}
@@ -208,49 +179,6 @@ void loop() {
 
 
 #ifdef DEBUG
-void loop_console() {
-	byte cmd;
-	static unsigned int pos = 0;
-	static unsigned int col1 = 0;
-	static unsigned int col2 = 0;
-	Serial.print("? ");
-	while (!Serial.available());
-	cmd = Serial.read();
-	switch (cmd) {
-	case '1':
-		myDFPlayer.playFolder(1, 1);  //play specific mp3 in SD:/15/004.mp3; Folder Name(1~99); File Name(1~255)    
-		col1 = col2 = 150;
-		break;
-	case '2': col2 = 150;
-		break;
-	case '3': col1 = 255;
-		break;
-	case '4': col2 = 255;
-		break;
-	case '0': pos = col1 = col2 = 0;
-		break;
-	case 'b':
-		myDFPlayer.playFolder(1, 2);  //play specific mp3 in SD:/15/004.mp3; Folder Name(1~99); File Name(1~255)    
-		pos = 70; col1 = col2 = 255;
-		break;
-	case 'n': pos = 10; col1 = col2 = 20;
-		break;
-	case 'm': pos = 35; col1 = col2 = 65;
-		break;
-	case 'v': pos = 0; col1 = col2 = 0;
-		break;
-	case 'M': pos = 180; col1 = col2 = 255;
-		break;
-	}
-
-	myServo.write(pos);              // tell servo to go to position in variable 'pos'
-	strip.setPixelColor(0, strip.Color(col1, 0, 0));
-	strip.setPixelColor(1, strip.Color(col2, 0, 0));
-	strip.show(); // This sends the updated pixel color to the hardware.
-	Serial.print("pos: "); Serial.println(pos);
-	Serial.print("col 1/2: "); Serial.print(col1); Serial.print("/"); Serial.println(col2);
-	delay(15);                       // waits 15ms for the servo to reach the position
-}
 
 void DFPlayer_printDetail(uint8_t type, int value) {
 	switch (type) {
